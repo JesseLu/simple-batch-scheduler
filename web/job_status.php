b0bb0c0968967af4e6103f7162ba4144c2e9078f
Jesse Lu <mr.jesselu@gmail.com>
2011-05-16 16:11:24 -0700

    Now safer, using escapeshellcmd().

--- web/job_status.php
+++ web/job_status.php
@@ -1,4 +1,4 @@
 <?php
 header("Cache-Control: no-cache");
-echo exec('/home/gce_demo/demo1/job_status' . $_POST["filename"])
+echo exec(escapeshellcmd('/home/gce_demo/demo1/job_status' . $_POST["filename"]));
 ?>
