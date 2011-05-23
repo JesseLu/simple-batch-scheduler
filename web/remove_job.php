7962cf84a1081b4637c5c93f84152ff2acb4b1ff
Jesse Lu <mr.jesselu@gmail.com>
2011-05-23 16:03:45 -0700

    Deletes file in work directory on user leave.

--- web/remove_job.php
+++ web/remove_job.php
@@ -1,8 +1,10 @@
 <?php
 header("Cache-Control: no-cache");
 $infile = '/home/gce_demo/demo1/in/'. trim($_POST["filename"]);
+$workfile = '/home/gce_demo/demo1/work/'. trim($_POST["filename"]);
 $outfile = '/home/gce_demo/demo1/out/'. trim($_POST["filename"]);
 unlink($infile);
+unlink($workfile);
 unlink($outfile);
 // echo '/home/gce_demo/demo1/out/' . $_POST["filename"]
 // echo $_POST["filename"]
